f7c19599592e31ca1e4e4148c7462885f5c33471
rabrigo <94808577+rabrigo@users.noreply.github.com>
2022-02-17 16:57:33 -0800

    Add files via upload

--- assets/project-links.js
+++ assets/project-links.js
@@ -1,5 +1,5 @@
 $('.project-1').click(function () {
-    window.location.replace("https://mweyer.github.io/First-Group-Project/");
+    window.location.assign("https://mweyer.github.io/First-Group-Project/");
     // $(this).attr("style", "background-color: white");
 })
 
@@ -8,3 +8,15 @@ $('.project-1').hover(function () {
     }, function () { 
         $(this).attr("style", "opacity: 1");
     })
+
+$('.project-2').click(function () {
+    window.location.assign("https://pacific-ravine-12135.herokuapp.com/");
+    // $(this).attr("style", "background-color: white");
+})
+
+$('.project-2').hover(function () {
+        $(this).attr("style", "opacity: .7");
+    }, function () { 
+        $(this).attr("style", "opacity: 1");
+    })
+
